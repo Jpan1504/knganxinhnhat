f820fc427596862bf5f5d2aae2c82d34703507ef
Jpan <nhathay1@gmail.com>
2021-10-04 09:47:46 +0700

    init file

---
+++ config.js
@@ -0,0 +1,18 @@
+// 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
+const CONFIG = {
+    introTitle: '🖐🖐🖐 Hè lô à nhon xê ô! 🖐🖐🖐',
+    introDesc: `Em có điều này muốn hỏi chị áaaaaa,chị trả lời đàng hoàng nhó.`,
+    btnIntro: 'OK luônn',
+    title: 'chị có biết nà em iu chị lắm hôn, nên nà 2 tụi mình đến với nhao nhoaa =)) 😙',
+    desc: 'chị không trả lời hoặc thoát ra, tức là chị đồng ý ròi đó nhá 😉 ',
+    btnYes: 'gòi gòi, chị đồng ý nèe 💖💖💖',
+    btnNo: '💔 em nằm mơ đi nháaa! 💔 ',
+    question:'nếu như chúng ta cùng đi dới nhao trên quãng đường về sau thì chị sẽ làm gì?😊😊',
+    reasonPlaceholder: 'kế hoạch',
+    btnReply: 'Gửi ! 💌💌',
+    reply: 'thì chúng ta sẽ cùng nhao ăn những bữa cơm, cùng nhao đùa vui, tươi cười thật thích, cùng nhao giúp đỡ mọi người bởi bàn tay tuyệt vời của chúng mình =)) và cùng nhao vượt qua mọi khó khăn và tiến lên nữa!',
+    mess: 'em biết mà, nhưng mà em sẽ làm nhiều điều hơn thế nữaaa 😘😘',
+    messDesc: 'hãy đợi em nhóoo, cái gì khó để Jpan lo nè 😉💬💬',
+    btnAccept: 'Okokkk nuôn kkkkkk💖',
+    messLink: 'https://m.me/PhungQuocDinh'
+}
